872fb61cdfca85a458d19fe3b59ee148cbd3f2a9
ephraim <ephraim@gtriipglobal.com>
2020-03-25 11:20:26 +0800

    [New][Sort] Shell Sort

--- src/ephraim/algo/sortings/Main.java
+++ src/ephraim/algo/sortings/Main.java
@@ -1,22 +1,30 @@
 package ephraim.algo.sortings;
 
 import ephraim.algo.helpers.SortHelper;
+import jdk.nashorn.tools.Shell;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Comparable[] arr = SortHelper.generateArr(0, 10, 1000);
+        Comparable[] arr = SortHelper.generateArr(0, 1000, 100000);
         Comparable[] copy1 = Arrays.copyOf(arr, arr.length);
+        Comparable[] copy2 = Arrays.copyOf(arr, arr.length);
 
         SortHelper.testSort("ephraim.algo.sortings.SelectionSort", arr);
         SortHelper.testSort("ephraim.algo.sortings.InsertionSort", copy1);
+        SortHelper.testSort("ephraim.algo.sortings.ShellSort", copy2);
+        //InsertionSort.show(copy2);
 
-        Comparable[] charr = SortHelper.generateArr("sampleexample");
+        /*Comparable[] charr = SortHelper.generateArr("sampleexample");
         SortHelper.testSort("ephraim.algo.sortings.InsertionSort", charr);
         InsertionSort.show(charr);
 
-        System.out.println(InsertionSort.isSorted(charr));
+        System.out.println(InsertionSort.isSorted(charr));*/
 
+        /*ShellSort.show(arr);
+        System.out.println("============ partition ============");
+        ShellSort.sort(arr);
+        ShellSort.show(arr);*/
     }
 }
--- src/ephraim/algo/sortings/ShellSort.java
+++ src/ephraim/algo/sortings/ShellSort.java
@@ -8,14 +8,27 @@ public class ShellSort extends SortInsatnce {
         int n = arr.length;
         int gap = n / 2;
 
-        for()
+        // only switch 1 time
+        while (gap >= 1) {
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j >=gap && arr[j].compareTo(arr[j - gap]) < 0; j -= gap) {
+                    swap(arr, j, j - gap);
+
+                }
+
+            }
+
+            gap /= 2;
+
+        }
+
+        // System.out.println("============= sorting finished =============");
 
     }
 
     public static void show(Comparable[] arr) {
         System.out.println("Shell sort showing result");
-        SortInsatnce.sort(arr);
-
+        SortInsatnce.show(arr);
 
     }
 }
